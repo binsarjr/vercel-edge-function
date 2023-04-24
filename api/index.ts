f429ac3e1e64c7cd9f63921c3395c49e116d7f82
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-04-24 07:34:15 +0700

    initial commit

---
+++ api/index.ts
@@ -0,0 +1,12 @@
+import type { RequestContext } from '@vercel/edge'
+ 
+export const config = {
+  runtime: 'edge',
+};
+ 
+export default function MyEdgeFunction(
+  request: Request,
+  context: RequestContext,
+) {
+  return new Response(`Hello, from ${request.url} I'm an Edge Function!`);
+}
